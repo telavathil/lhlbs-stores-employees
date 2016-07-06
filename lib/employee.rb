8680650dfb1f1d031fec07908a02ef6969e16b31
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-07-06 13:16:14 -0400

    ex7 done

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates_presence_of :first_name,
+  validates_presence_of :last_name,
+  validates_numericality_of :hourly_rate, :inclusion => {:in => [40,200]}
+  validates_associated :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}, presense: true
+  validates :annual_revenue, numericality: {greater_than: 0}
+  vadilate :both_apperal
+
+  private
+  def both_apperal
+     if !(self.mens_apparel || self.womens_apparel)
+       self.errors.add(:base,"Store must sell at least one form of apparel.")
+     end
+  end
 end
