dfeab1aed4bd402edff943316f05a235122d000e
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-07-06 12:40:30 -0400

    ex6 done

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,9 +7,9 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-surrey = Store.create(name:'Surrey',annual_revenue:224000,mens_apparel:false,womens_apparel:true)
-whistler = Store.create(name:'whistler',annual_revenue:1900000,mens_apparel:true,womens_apparel:false)
-yaletown = Store.create(name:'yaletown',annual_revenue:430000,mens_apparel:true,womens_apparel:true)
+@store4 = Store.create(name:'Surrey',annual_revenue:224000,mens_apparel:false,womens_apparel:true)
+@store5 = Store.create(name:'whistler',annual_revenue:1900000,mens_apparel:true,womens_apparel:false)
+@store6 = Store.create(name:'yaletown',annual_revenue:430000,mens_apparel:true,womens_apparel:true)
 
 @mens_stores = Store.where('mens_apparel = true')
 @mens_stores.each {|store| puts "#{store.name}: does #{store.annual_revenue} a year in revenue"}
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Michael", last_name: "Jordan", hourly_rate: 60)
+@store2.employees.create(first_name: "Steph", last_name: "Curry", hourly_rate: 60)
+@store2.employees.create(first_name: "LeBron", last_name: "James", hourly_rate: 60)
+
+@store4.employees.create(first_name: "Alan", last_name: "Turing", hourly_rate: 60)
+@store4.employees.create(first_name: "Ada", last_name: "Lovelace", hourly_rate: 60)
+@store4.employees.create(first_name: "Steve", last_name: "Wozniak", hourly_rate: 60)
+
+binding.pry
+puts Employee.count
